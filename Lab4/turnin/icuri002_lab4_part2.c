0a02d3ebb6d3f1e1a2d14df91b515a251101d823
Isaac Curiel <icuri002@wch136-34.cs.ucr.edu>
2020-04-19 14:10:05 -0700

    Completed part 2

---
+++ Lab4/turnin/icuri002_lab4_part2.c
@@ -0,0 +1,138 @@
+/*	Author: icuri002
+ *  Partner(s) Name: 
+ *	Lab Section: 024
+ *	Assignment: Lab #4  Exercise #2
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+enum States {Start, Init, Increment, Decrement, Reset, Wait, Both_Released} state;
+unsigned char A;
+unsigned char tmpC;
+
+void tick() {
+	switch(state) {
+		case Start:
+			state = Init;
+			break;
+
+		case Init:
+			if (A == 0x01) {
+				state = Increment;
+			}
+			else if (A == 0x02) {
+				state = Decrement;
+			}
+			else if (A == 0x03) {
+				state = Reset;
+			}
+			else if (A == 0x00) {
+				state = Init;
+			}
+			break;
+
+	// Left off: Where to initialize C to 0x07? Also is it C or tmpC?
+		case Increment:
+			state = Wait;
+			break;
+
+		case Decrement:
+			state = Wait;
+			break;
+
+		case Wait:
+			if (A == 0x03) {
+				state = Reset;
+			}
+			else if (A == 0x00) {
+				state = Both_Released;
+			}
+			else {
+				state = Wait;
+			}
+			break;
+
+		case Both_Released:
+			if (A == 0x03) {
+				state = Reset;
+			}
+			else if (A == 0x01) {
+				state = Increment;
+			}
+			else if (A == 0x02) {
+				state = Decrement;
+			}
+			else if (A == 0x00) {
+				state = Both_Released;
+			}
+			break;
+
+		case Reset:
+			if (A == 0x01) {
+				state = Increment;
+			}
+			else if (A == 0x02) {
+				state = Decrement;
+			}
+			else if (A == 0x03) {
+				state = Reset;
+			}
+			else if (A == 0x00) {
+				state = Both_Released;
+			}
+			break;
+
+		default:
+			state = Start;
+			break;
+	}
+	
+	switch(state) {
+		case Init:
+			tmpC = 0x07;	
+			break;
+
+		case Increment:
+			if (tmpC < 9) {
+				tmpC = tmpC + 1;
+			}
+			break;
+
+		case Decrement:
+			if (tmpC > 0) {
+				tmpC = tmpC - 1;
+			}
+			break;
+
+		case Reset:
+			tmpC = 0x00;
+			break;
+
+		default:
+			break;
+	}
+}
+
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+    DDRA = 0x00; PORTA = 0xFF;	// Configure as inputs
+    DDRC = 0xFF; PORTC = 0x00;	// Configure as outputs; initialize C to 7	
+
+    state = Start;
+    tmpC = 0x07; 
+   
+    /* Insert your solution below */
+    while (1) {
+	A = PINA & 0x03;
+	tick();	
+	PORTC = tmpC;
+    }
+    return 1;
+}
